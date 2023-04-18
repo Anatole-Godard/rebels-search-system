1f36489d4f58f3f5d4f99ed68e81327f2ed63fa6
anatole <anatole.godard@icloud.com>
2023-04-18 19:29:40 +0200

    feat(index.js): use the utils

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
+import dotenv from 'dotenv'
 import express from 'express'
 
+import loginRouter from './utils/login.utils.js'
+import {getTokenAndValidAccess} from './utils/token.utils.js';
+
+dotenv.config()
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
+
+app.use(loginRouter)
+
+app.use(getTokenAndValidAccess)
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
